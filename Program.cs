dc6ae6af34ebe14072fee86fd71609e014af7d08
Никита К <fox643@yandex.ru>
2022-12-08 11:46:35 +0300

    первый коммит

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.Clear();
+int FindSecond(int var)
+{
+    Console.Write("Введите трехзначное число чтобы узнать его вторую цифру ");
+    var = int.Parse(Console.ReadLine());
+    return var;
+}
+int var = 0;
+int secondNumb = FindSecond(var);
+secondNumb = secondNumb / 10 % 10;
+Console.WriteLine(secondNumb);
